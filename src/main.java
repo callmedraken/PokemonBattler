f65b6a2d79a8837fc44b7596bc0159921eb7c2c3
John <jhsiao1996@gmail.com>
2019-07-17 17:32:20 -0700

    Signed-off-by: John <jhsiao1996@gmail.com>

--- src/main.java
+++ src/main.java
@@ -2,6 +2,11 @@ public class main
 {
     public static void main(String[] args)
     {
-        
+        menu();
+    }
+
+    public static void menu()
+    {
+        System.out.println("Welcome to Pokemon Battler");
     }
 }
