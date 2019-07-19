4920b9419efe6223ae788c62a613c104686218c0
John <jhsiao1996@gmail.com>
2019-07-18 17:46:56 -0700

    Added pokemon types

--- src/PokemonInfo.java
+++ src/PokemonInfo.java
@@ -2,6 +2,6 @@ public class PokemonInfo
 {
     public PokemonInfo()
     {
-        
+
     }
 }
--- src/PokemonType.java
+++ src/PokemonType.java
@@ -1,4 +1,41 @@
-public class PokemonType
+public enum PokemonType
 {
+    NORMAL(),
+
+    FIRE(),
+
+    FIGHTING(),
+
+    WATER(),
+
+    FLYING(),
+
+    GRASS(),
+
+    POISON(),
+
+    ELECTRIC(),
+
+    GROUND(),
+
+    PSYCHIC(),
+
+    ROCK(),
+
+    ICE(),
+
+    BUG(),
+
+    DRAGON(),
+
+    GHOST(),
+
+    DARK(),
+
+    STEEL(),
+
+    FAIRY();
+
+
 
 }
